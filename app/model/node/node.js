01199db15ab55dc523d7c382d0f9983549ab7968
Reinder Kamphorst <reinder@toubab.nl>
2012-11-26 22:04:10 +0100

    scribble

--- app/model/node/node.js
+++ app/model/node/node.js
@@ -11,7 +11,7 @@
         var NOT_IMPLEMENTED = new Error ("NOT IMPLEMENTED");
         var rootNode = null;
         
-        function Node(id, seq, title, date, info, parent, fetcher) {
+        function Node(id, seq, title, date, info, parent) {
             this._id = id;
             this._title = title;
             this._date = date;
@@ -21,14 +21,6 @@
             
             this._parent = parent ? parent : null;
             this._generation = null;
-            
-            if (fetcher) {
-                this._fetcher = fetcher;
-            } else if (parent) {
-                this._fetcher = parent._fetcher;
-            } else {
-                this._fetcher = {};
-            }
             
             this._children = null;
             this._seq = seq ? seq : 0;
@@ -95,15 +87,6 @@
         Node.prototype.setArticle = function setArticle(content) {
             this._article = content;
         };
-
-        Node.prototype.clearArticle = function clearArticle() {
-            this.setArticle(null);
-        };
-        
-        Node.prototype.getChildren = function getChildren() {
-            // return a *copy* of the _children internal array by using .concat()
-            return this._children ? this._children.concat() : null;
-        };
         
         Node.prototype.getDescendants = function getDescendants(depth) {
             
@@ -129,6 +112,11 @@
             return descendants;   
         };
         
+        Node.prototype.getChildren = function getChildren() {
+            // return a copy of the _children internal array by using .concat()
+            return this._children ? this._children.concat() : null;
+        };
+        
         Node.prototype.setChildren = function setChildren(items) {
             if (this._children !== null) {
                 for (var i = 0; i < this._children.length; i++) {
@@ -146,77 +134,145 @@
             }
         };
         
-        Node.prototype.clearChildren = function clearChildren() {
-            this.setChildren(null);
-        };
-        
-        Node.prototype.createChild = function createChild(id, seq, title, date, info) {
-            return new Node(id, seq, title, date, info, this);
-        };
-        
         Node.prototype.toString = function toString() {
             return "Node " + this._id + ": " + this._title;
         };
         
-        Node.prototype.withDescendants = function withDescendants(depth) {
-            var params = this.queryFetchTreeParams(depth);
-            var self = this;
+
+        Node.prototype.withArticle = function() {
+            return withArticle([ this ]).then(function(nodes) { return nodes[0] });
+        };
+        
+        function withArticle (nodes) {
+            var nids = [];
+            var ndict = {};
+            for (var i = 0; i < nodes.length; i++) {
+                
+                var article = node.getArticle();
+                
+                if (article == null) {
+                    var nid = node.getId();
+                    nids.push(nid);
+                    ndict[nid] = node;
+                }
+            }
+            
+            if (nids.length > 0) {
+                return when(fetchContent(nids))
+                    .then(function(dict) {
+                        for (var nid in dict) {
+                            ndict[nid].setArticle(dict[nid]);
+                        }
+                        return nodes;
+                    });
+            } else {
+                return when(nodes);
+            }
+        };        
+
+        function withDescendants(nodes, depth) {
+            var params = queryFetchTreeParams(nodes, depth);
             
             if (params.nids.length > 0) {
                 return
-                    when(this.fetchTrees(params.nids, params.depths))
+                    when(fetchTrees(params.nids, params.depths))
                         .then(function(trees) {
-                            self.attachFetchedTrees(trees);
-                            return self.getDescendants();
+                            attachFetchedTrees(nodes, trees);
+                            return getAllDescendants(nodes, trees);
                         });
             } else {
-                return when (self.getDescendants());
-            } 
-        };
+                return when (getAllDescendants(nodes, trees));
+            }
+        }
         
-        Node.prototype.withDescendantContent = function withDescendantContent(depth) {
-            var self = this;
-            return this
-                .withDescendants(depth)
+        function withDescendantContent(node, depth) {
+            return 
+                withDescendants(node, depth)
                 .then(function(descendants) {
-                    var params = descendants
+                    var nids = descendants
                         .map(function(node) { return node.getArticle() !== null ? node.getId() : null; })
                         .filter(function(id) { return id !== null; });
                     
-                    if (params.length > 0) {
-                        return when(self.fetchContent(params))
+                    if (nids.length > 0) {
+                        return when(fetchContent(nids))
                                 .then(function(dict) {
-                                    self.attachFetchedContent(dict);
-                                    return self.getDescendants(depth);
+                                    attachFetchedContent(node, dict);
+                                    return node.getDescendants(depth);
                                 });
                     } else {
                         return when(descendants);
                     }
                 });
-        };
-        
-        Node.prototype.withChildren = function withChildren() {
-            return this.withDescendants(1);
-        };
-
-        Node.prototype.withChildArticles = function withChildArticles() {
-            return this.withDescendantContent(1);
         }
-
-        Node.prototype.withArticle = function withArticle() {
+        
+         function getAllDescendants(nodes, depth) {
+            var result = [];
+            for (var i = 0; i < nodes.length; i++) {
+                var nresult = nodes[i].getDescendants(depth);
+                result = (nresult === null ? null : result.concat(nodes[i].getDescendants(depth)));
+                if (result === null) break;
+            }
+            return result;
+        }
+            
+        function attachFetchedTrees  (node, trees) {
+            var myId = node.getId();
+            var attachedAll = true;
+            for (var i = 0; i < trees.length; i++) {
+                var tree = trees[i];
+                if (tree.getId() === myId) {
+                    node.setChildren(tree.getChildren();
+                } else {
+                    var children = node.getChildren();
+                    var attached = false;
+                    if (children !== null && children.length > 0) {
+                        for (var j = 0; j < children.length; j++) {
+                            if (attachFetchedTrees(children[j], [ tree ])) {
+                                attached = true;
+                                break;
+                            } 
+                        }
+                    }
+                    
+                    if (!attached)
+                        attachedAll = false;
+                }
+            }
+            return attachedAll;
+        }
+        
+        function attachFetchedContent(node, dict) {
             var myId = this.getId();
-            var self = this;
-            return when(this.fetchContent([ myId ]))
-                .then(function(dict) {
-                    self.setArticle(dict[myId]);
-                    return self;
-                });
-        };        
-
-        Node.prototype.queryFetchTreeParams = function(depth) {
-            if (this._children === null) {
+            var attachedAll = true;
+            for (var id in dict) {
+                if (id == myId) {
+                    node.setArticle(dict[id]);
+                } else {
+                    var children = node.getChildren();
+                    var attached = false;
+                    if (children != null && children.length > 0) {
+                        var param = {};
+                        param[id] = dict[id];
+                        for (var i = 0; i < children.length; i++) {
+                            if (attachFetchedContent(children[i], param)) {
+                                attached = true;
+                                break;
+                            }
+                        }
+                    }
+                    
+                    if (!attached)
+                        attachedAll = false;
+                }
+            }
+            return attachedAll;
+        }
+        
+        function queryFetchTreeParams(node, depth) {
+            var children = node.getChildren();
+            if (children === null) {
                 return {
-                    nids: [ this.getId() ],
+                    nids: [ node.getId() ],
                     depths: [ depth ]
                 };
             } else {
@@ -225,8 +281,8 @@
                     depths: []
                 };
                 if (depth > 0) {
-                    for (var i = 0; i < this._children.length; i++) {
-                        var cresult = this._children[i].queryFetchTreeParams(depth - 1);
+                    for (var i = 0; i < children.length; i++) {
+                        var cresult = queryFetchTreeParams(children[i], depth - 1);
                         result.nids = result.nids.concat(cresult.nids);
                         result.depths = result.depths.concat(cresult.depths);
                     }
@@ -235,62 +291,31 @@
             }
         };
         
-        Node.prototype.attachFetchedTrees = function(trees) {
-            var myId = this.getId();
-            var attachedAll = true;
-            for (var i = 0; i < trees.length; i++) {
-                var tree = trees[i];
-                if (tree.getId() == myId) {
-                    this.setChildren(tree.getChildren();
-                } else if (this._children !== null && this._children.length > 0) {
-                    for (var j = 0; j < this._children.length; j++) {
-                        if (!this._children[j].attachFetchedTrees([ tree ])) {
-                            attachedAll = false;
-                        } else {
-                            attachedAll = true;
-                            break;
-                        }
-                    }
+        function fetchTrees(nids, depths, fetcher) {
+            var deferred = when.defer();
+            
+            try {
+                if (typeof fetcher.fetchTrees == "function") {
+                    var callback = deferred.resolve;
+                    var errback = deferred.reject;
+                    fetcher.fetchTrees(self, nids, depths, callback, errback);
                 } else {
-                    attachedAll = false;
+                    deferred.reject(NOT_IMPLEMENTED);
                 }
+            } catch (ex) {
+                deferred.reject(ex);
             }
-            return attachedAll;
-        };
-        
-        Node.prototype.attachFetchedContent = function(dict) {
-            var myId = this.getId();
-            var attachedAll = true;
-            for (var id in dict) {
-                if (id == myId) {
-                    this.setArticle(dict[id]);
-                } else if (this._children.length != null && this._children.length > 0) {
-                    var param = {};
-                    param[id] = dict[id];
-                    for (var i = 0; i < this._children.length; i++) {
-                        if (this._children.attachFetchedContent(param)) {
-                            attachedAll = true;
-                            break;
-                        } else {
-                            attachedAll = false;
-                        }
-                    }
-                } else {
-                    attachedAll = false;
-                }
-            }
-            return attachedAll;
+            return deferred.promise;
         }
         
-        Node.prototype.fetchTrees = function fetchTrees(nids, depths) {
-            var self = this;
+        function fetchContent(nids) {
             var deferred = when.defer();
             
             try {
-                if (typeof this._fetcher.fetchTrees == "function") {
+                if (typeof fetcher.fetchContent == "function") {
                     var callback = deferred.resolve;
                     var errback = deferred.reject;
-                     this._fetcher.fetchTrees(self, nids, depths, callback, errback);
+                    fetcher.fetchContent(self, nids, callback, errback);
                 } else {
                     deferred.reject(NOT_IMPLEMENTED);
                 }
@@ -300,15 +325,14 @@
             return deferred.promise;
         };
         
-        Node.prototype.fetchContent = function fetchContent(nids) {
-            var self = this;
+        function fetchNodes(props) {
             var deferred = when.defer();
             
             try {
-                if (typeof this._fetcher.fetchContent == "function") {
+                if (typeof fetcher.fetchNodes == "function") {
                     var callback = deferred.resolve;
                     var errback = deferred.reject;
-                    this._fetcher.fetchContent(self, nids, callback, errback);
+                    fetcher.fetchNodes(self, props, callback, errback);
                 } else {
                     deferred.reject(NOT_IMPLEMENTED);
                 }
@@ -316,41 +340,6 @@
                 deferred.reject(ex);
             }
             return deferred.promise;
-        };
-        
-        Node.prototype.fetchNodes = function fetchNodes(props) {
-            var self = this;
-            var deferred = when.defer();
-            
-            try {
-                if (typeof this._fetcher.fetchNodes == "function") {
-                    var callback = deferred.resolve;
-                    var errback = deferred.reject;
-                     this._fetcher.fetchNodes(self, props, callback, errback);
-                } else {
-                    deferred.reject(NOT_IMPLEMENTED);
-                }
-            } catch (ex) {
-                deferred.reject(ex);
-            }
-            return deferred.promise;
-        };
-        
-        Node.withRootNode = function fetchRootNode(fetcher, depth) {
-            if (rootNode != null) {
-                return when(rootNode.withDescendants(depth));
-            } else {
-                var fakeNode = new Node(-1, 0, null, null, null, null, fetcher);
-                return 
-                    fakeNode.fetchTrees([ 0 ], depth + 1)
-                        .then(function(trees)) {
-                            if (trees.length == 0) {
-                                throw new Error ("No root node with id = 0!");
-                            }
-                            rootNode = trees[0];
-                            return rootNode;
-                        });
-            }
         };
     });
     
